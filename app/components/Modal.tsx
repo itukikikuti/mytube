1f9537f0d7e10edb1700723e097ea1809eaed13a
itukikikuti <xxvraqwa@gmail.com>
2026-01-04 05:16:56 +0000

    docker-compose.ymlの追加と動画ソースの修正: 動画ファイルのパスを更新し、docker-compose.ymlを新規作成

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -75,7 +75,8 @@ export function Modal({ item, onClose, onAddThumb, onRemoveThumb, onRate, onOpen
               autoPlay
               loop
             >
-              <source src={`/api/stream-video?filename=${item ? encodeURIComponent(item.title) : ''}`} type="video/mp4" />
+              <source src={`/videos/${item ? encodeURIComponent(item.title) : ''}`} type="video/mp4" />
+              {/* <source src={`/api/stream-video?filename=${item ? encodeURIComponent(item.title) : ''}`} type="video/mp4" /> */}
               お使いのブラウザは video をサポートしていません。
             </video>
           </div>
