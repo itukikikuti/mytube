e9fe2c37f43fbf70ac2792ff0b256d2f49a323a1
itukikikuti <xxvraqwa@gmail.com>
2026-06-07 21:50:20 +0900

    html配信

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.static("public"));
+
+app.get("/api/message", (req, res) => {
   res.send("Hello, World!");
 });
 
