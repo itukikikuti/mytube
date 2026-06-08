c184ef84cb94e5d1254521fcbbc9c2a3b87dc638
itukikikuti <xxvraqwa@gmail.com>
2026-06-08 21:15:18 +0900

    thenをawaitに書き換え

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,6 +1,7 @@
-fetch("/api/media-items")
-  .then(res => res.text())
-  .then(message => {
-    document.getElementById("message")!.textContent = message;
-  });
-  
+async function main() {
+  const res = await fetch("/api/media-items");
+  const message = await res.text();
+  document.getElementById("message")!.textContent = message;
+}
+
+main();
