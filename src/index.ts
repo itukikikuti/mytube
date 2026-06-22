eb44c42a3b6b4c1ee0fc38aadf634a5303857711
itukikikuti <xxvraqwa@gmail.com>
2026-06-22 21:34:26 +0900

    メディアが画面に出たら詳細を取得

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import { serveStatic } from '@hono/node-server/serve-static'
 import { Hono } from 'hono'
 import Database from "better-sqlite3"
 
+type MediaItem = {
+  id: number
+  title: string
+  date: string
+  type: string
+  duration: string
+  rate: number
+}
+
 const db: Database.Database = new Database("data/db.sqlite")
 
 const app = new Hono()
@@ -13,10 +22,26 @@ app.use('/videos/*', serveStatic({ root: './' }))
 app.get('/', serveStatic({ path: './static/index.html' }))
 
 app.get("/medias", (c) => {
-  const rows = db.prepare("SELECT id,title,date,type,duration,rate FROM media_items").all();
-  return c.html(rows.map((row: any) => `
-    <li id="media-${row.id}" onclick="playVideo('/videos/${encodeURIComponent(row.title)}')">${row.title}</li>
+  const rows = db.prepare<[], Pick<MediaItem, "id" | "title">>("SELECT id,title FROM media_items").all();
+  return c.html(rows.map((row) => `
+    <div id="media-${row.id}" data-media-id="${row.id}" onclick="playVideo('/videos/${encodeURIComponent(row.title)}')"></div>
   `).join(''))
+})
+
+app.get("/medias/:id", (c) => {
+  const row = db.prepare<[string], MediaItem>("SELECT * FROM media_items WHERE id = ?").get(c.req.param("id"));
+
+  if (!row) {
+    return c.notFound()
+  }
+
+  return c.html(`
+    <p>${row.title}</p>
+    <p>${row.date}</p>
+    <p>${row.type}</p>
+    <p>${row.duration}</p>
+    <p>${row.rate}</p>
+  `)
 })
 
 const port = Number(process.env.PORT ?? 3000)
